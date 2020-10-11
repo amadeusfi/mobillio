0071114e443fd4bf349cfb504f771779d4d00e9a
Amadeus Figueiredo <62649956+amadeusfi@users.noreply.github.com>
2020-10-11 21:18:43 +0200

    Delete fizzbuzz.php

--- fizzbuzz.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-// Aufgabe: Mach ein Program, das von 1 bis 99 zaehlt
-// und die Zahlen ausgibt, eine pro Zeile.
-// wenn die Zahl durch 3 teilbar ist schreibe statt der Zahl "Fizz"
-// wenn die Zahl druch 5 teilbar ist, schreibe statt der Zahl "Buzz"
-// wenn die Zahl durch 3 und druch 5 teilbar ist, schreibe Fizzbuzz
-// so wenig code wie moeglich 
