5045f0951c73e27f2e9af28da7a1f2b9eb630b79
amadeusfi <amadeus_figueiredo@protonmail.com>
2020-10-13 15:33:37 +0200

    footer & fuctionality

--- index.php
+++ index.php
@@ -193,7 +193,233 @@
     </div>
   </div>
   <!-- End Advantages -->
-  
+  <!-- Hot -->
+  <div id="hot">
+    <div class="box">
+      <div class="container">
+        <div class="col-md-12">
+          <h2>
+            Our Latest Products
+          </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Hot -->
+  <!-- Products -->
+  <div id="content" class="container">
+    <div class="row">
+      <!-- col-sm-4 col-sm-6 single  -->
+      <div class="col-sm-4 col-sm-6 single">
+        <div class="product">
+          <a href="details.php">
+            <img class="img-responsive" src="admin_area/product_images/armabam.jpg" alt="Product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">
+                Armario Bambini
+              </a>
+            </h3>
+            <p class="price">$30</p>
+            <p class="button">
+              <a href="details.php" class="btn btn-default">View Details</a>
+              <a href="details.php" class="btn btn-primary">
+                <i class="fa fa-shopping-cart">
+                  Add To Cart
+                </i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col-sm-4 col-sm-6 single  -->
+      <!-- col-sm-4 col-sm-6 single  -->
+      <div class="col-sm-4 col-sm-6 single">
+        <div class="product">
+          <a href="details.php">
+            <img class="img-responsive" src="admin_area/product_images/armabam.jpg" alt="Product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">
+                Armario Bambini
+              </a>
+            </h3>
+            <p class="price">$30</p>
+            <p class="button">
+              <a href="details.php" class="btn btn-default">View Details</a>
+              <a href="details.php" class="btn btn-primary">
+                <i class="fa fa-shopping-cart">
+                  Add To Cart
+                </i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col-sm-4 col-sm-6 single  -->
+      <!-- col-sm-4 col-sm-6 single  -->
+      <div class="col-sm-4 col-sm-6 single">
+        <div class="product">
+          <a href="details.php">
+            <img class="img-responsive" src="admin_area/product_images/armabam.jpg" alt="Product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">
+                Armario Bambini
+              </a>
+            </h3>
+            <p class="price">$30</p>
+            <p class="button">
+              <a href="details.php" class="btn btn-default">View Details</a>
+              <a href="details.php" class="btn btn-primary">
+                <i class="fa fa-shopping-cart">
+                  Add To Cart
+                </i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col-sm-4 col-sm-6 single  -->
+      <!-- col-sm-4 col-sm-6 single  -->
+      <div class="col-sm-4 col-sm-6 single">
+        <div class="product">
+          <a href="details.php">
+            <img class="img-responsive" src="admin_area/product_images/armabam.jpg" alt="Product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">
+                Armario Bambini
+              </a>
+            </h3>
+            <p class="price">$30</p>
+            <p class="button">
+              <a href="details.php" class="btn btn-default">View Details</a>
+              <a href="details.php" class="btn btn-primary">
+                <i class="fa fa-shopping-cart">
+                  Add To Cart
+                </i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col-sm-4 col-sm-6 single  -->
+      <!-- col-sm-4 col-sm-6 single  -->
+      <div class="col-sm-4 col-sm-6 single">
+        <div class="product">
+          <a href="details.php">
+            <img class="img-responsive" src="admin_area/product_images/armabam.jpg" alt="Product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">
+                Armario Bambini
+              </a>
+            </h3>
+            <p class="price">$30</p>
+            <p class="button">
+              <a href="details.php" class="btn btn-default">View Details</a>
+              <a href="details.php" class="btn btn-primary">
+                <i class="fa fa-shopping-cart">
+                  Add To Cart
+                </i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col-sm-4 col-sm-6 single  -->
+      <!-- col-sm-4 col-sm-6 single  -->
+      <div class="col-sm-4 col-sm-6 single">
+        <div class="product">
+          <a href="details.php">
+            <img class="img-responsive" src="admin_area/product_images/armabam.jpg" alt="Product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">
+                Armario Bambini
+              </a>
+            </h3>
+            <p class="price">$30</p>
+            <p class="button">
+              <a href="details.php" class="btn btn-default">View Details</a>
+              <a href="details.php" class="btn btn-primary">
+                <i class="fa fa-shopping-cart">
+                  Add To Cart
+                </i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col-sm-4 col-sm-6 single  -->
+      <!-- col-sm-4 col-sm-6 single  -->
+      <div class="col-sm-4 col-sm-6 single">
+        <div class="product">
+          <a href="details.php">
+            <img class="img-responsive" src="admin_area/product_images/armabam.jpg" alt="Product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">
+                Armario Bambini
+              </a>
+            </h3>
+            <p class="price">$30</p>
+            <p class="button">
+              <a href="details.php" class="btn btn-default">View Details</a>
+              <a href="details.php" class="btn btn-primary">
+                <i class="fa fa-shopping-cart">
+                  Add To Cart
+                </i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col-sm-4 col-sm-6 single  -->
+      <!-- col-sm-4 col-sm-6 single  -->
+      <div class="col-sm-4 col-sm-6 single">
+        <div class="product">
+          <a href="details.php">
+            <img class="img-responsive" src="admin_area/product_images/armabam.jpg" alt="Product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">
+                Armario Bambini
+              </a>
+            </h3>
+            <p class="price">$30</p>
+            <p class="button">
+              <a href="details.php" class="btn btn-default">View Details</a>
+              <a href="details.php" class="btn btn-primary">
+                <i class="fa fa-shopping-cart">
+                  Add To Cart
+                </i>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col-sm-4 col-sm-6 single  -->
+    </div>
+  </div>
+
+  <!-- End Products -->
+  <!-- Footer -->
+  <?php
+  include("includes/footer.php"); 
+  ?>
+  <!-- End Footer -->
+
+
 
   <script type="text/javascript" src="js/jquery-331.min.js"></script>
 
